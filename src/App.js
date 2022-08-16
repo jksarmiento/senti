f9f23b24e94a92e52e8339ae8ed9483f6d68dcb3
Jarl Sarmiento <jarlksarmiento@gmail.com>
2022-08-17 03:28:43 +0800

    cleaned project and added prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a href="https://reactjs.org">Learn React</a>
+                <Button>Hello World!</Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
